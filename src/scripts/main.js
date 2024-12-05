e56ad56c4a2fcd3a3001f931edd31eb163f9bf80
LucasVTK <lucazim.vtk@gmail.com>
2024-12-05 15:48:45 -0300

    concertando código

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document
-        .getElementById('form-sorteador')
-        .addEventListener('submit', function(evento) {
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
             evento.preventDefault()
             let numMax = document.getElementById('numero-maximo').value
             numMax = parseInt(numMax)
@@ -9,9 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let numRandom = Math.random() * numMax
             numRandom = Math.floor(numRandom + 1)
 
-            document
-                .getElementById('resultado-valor')
-                .innerText = numRandom
+            document.getElementById('resultado-valor').innerText = numRandom
             document.querySelector('.resultado').style.display = 'block'
         })
 })
